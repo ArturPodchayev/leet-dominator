7d618d86f15531da9f1f098ee39a5de60d7fc193
Artur <142787497+ArturPodchayev@users.noreply.github.com>
2026-06-17 16:18:44 +0500

    Create solution.cs

---
+++ the-kth-factor-of-n-/solution.cs
@@ -0,0 +1,15 @@
+public class Solution {
+    public int KthFactor(int n, int k) {
+        
+        if(k==1) return 1;
+        int count=1;
+        for(int i=2;i<=n;i++)
+        {
+            if(n%i==0)
+               count++;
+            if(count==k)
+               return i;
+        }
+        return -1;
+    }
+}

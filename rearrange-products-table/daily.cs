6bc6b27c3e1ee50a46e7d00777d30769b9c7a43e
Artur <142787497+ArturPodchayev@users.noreply.github.com>
2026-06-19 16:35:43 +0500

    Create daily.cs

---
+++ rearrange-products-table/daily.cs
@@ -0,0 +1,11 @@
+public class Solution {
+    public int LargestAltitude(int[] gain) {
+        int maxAltitude = 0;
+        int currentAltitude = 0;
+        foreach (int g in gain) {
+            currentAltitude += g;
+            maxAltitude = Math.Max(maxAltitude, currentAltitude);
+        }
+        return maxAltitude;        
+    }
+}

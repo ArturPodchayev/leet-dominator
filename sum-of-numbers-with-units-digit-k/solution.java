5564cb0d4ac9d2a7323215d5f51cb0babd3665b9
Artur <142787497+ArturPodchayev@users.noreply.github.com>
2026-07-17 13:48:53 +0500

    Create solution.java

---
+++ sum-of-numbers-with-units-digit-k/solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int minimumNumbers(int num, int k) {
+        if (num == 0) return 0;
+
+        for (int cnt = 1; cnt <= num; cnt++) {
+            if ((cnt * k) % 10 == num % 10 && cnt * k <= num) {
+                return cnt;
+            }
+        }
+        return -1;
+    }
+}

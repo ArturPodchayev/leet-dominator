7bd24f858bb858fa5fe4925f4c68b8810c8ae594
Artur <142787497+ArturPodchayev@users.noreply.github.com>
2026-07-31 17:51:39 +0500

    Create solution.go

---
+++ decode-the-message/solution.go
@@ -0,0 +1,15 @@
+func decodeMessage(key string, message string) string {
+	table := map[rune]byte{' ': ' '}
+	for _, c := range key {
+		if _, ok := table[c]; !ok {
+			table[c] = byte(len(table) - 1 + 'a')
+		}
+	}
+
+	var buffer bytes.Buffer
+	for _, c := range message {
+		buffer.WriteByte(table[c])
+	}
+
+	return buffer.String()
+}

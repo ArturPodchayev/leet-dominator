6e2f021f9af27d2bf004802b4417f3c081bb6207
Artur <142787497+ArturPodchayev@users.noreply.github.com>
2026-08-03 13:23:46 +0500

    Create daily.php

---
+++ stone-game-vii/daily.php
@@ -0,0 +1,26 @@
+class Solution {
+    public function stoneGameIII(array $stoneValue): string {
+        $n = count($stoneValue);
+
+        $dp = [];
+
+        for ($i = $n - 1; $i >= 0; $i--) {
+            $dp[$i] = $stoneValue[$i] - $dp[$i + 1];
+
+            if ($i + 2 <= $n)
+                $dp[$i] = max($dp[$i], $stoneValue[$i] + $stoneValue[$i + 1] - $dp[$i + 2]);
+
+            if ($i + 3 <= $n)
+                $dp[$i] = max($dp[$i], $stoneValue[$i] + $stoneValue[$i + 1] + $stoneValue[$i + 2] - $dp[$i + 3]);
+
+        }
+
+        if ($dp[0] > 0)
+            return "Alice";
+
+        if ($dp[0] < 0)
+            return "Bob";
+
+        return "Tie";
+    }
+}

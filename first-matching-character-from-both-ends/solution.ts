75c93ca6c54172529aa35c58100c1b283a2723d1
Artur <142787497+ArturPodchayev@users.noreply.github.com>
2026-08-08 20:40:29 +0500

    Create solution.ts

---
+++ first-matching-character-from-both-ends/solution.ts
@@ -0,0 +1,8 @@
+function firstMatchingIndex(s: string): number {
+    for(let i = 0; i < s.length/2; i++){
+        if (s[i] === s[s.length-i-1]){
+            return i;
+        }
+    }
+    return -1;
+};

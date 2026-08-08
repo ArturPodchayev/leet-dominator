0bd0e60751ec3ac70c2e8e19105b27c5cec9dfa2
Artur <142787497+ArturPodchayev@users.noreply.github.com>
2026-08-08 20:39:38 +0500

    Create solution.py

---
+++ first-matching-character-from-both-ends/solution.py
@@ -0,0 +1,10 @@
+class Solution:
+    def firstMatchingIndex(self, s: str) -> int:
+        l = 0
+        r = len(s) - 1
+        while l <= r:
+            if s[l] == s[r]:
+                return l
+            l = l + 1
+            r = r - 1
+        return -1

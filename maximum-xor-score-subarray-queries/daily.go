7059705dde97b2c2b0913a02d24a5ec0e5e68956
Artur <142787497+ArturPodchayev@users.noreply.github.com>
2026-08-16 18:12:49 +0500

    Create daily.go

---
+++ maximum-xor-score-subarray-queries/daily.go
@@ -0,0 +1,20 @@
+func stoneGameIX(stones []int) bool {
+	count := [3]int{}
+
+	for _, stone := range stones {
+		count[stone%3]++
+	}
+
+	if count[0]%2 == 0 {
+		return count[1] > 0 && count[2] > 0
+	}
+
+	return abs(count[1]-count[2]) > 2
+}
+
+func abs(value int) int {
+	if value < 0 {
+		return -value
+	}
+	return value
+}

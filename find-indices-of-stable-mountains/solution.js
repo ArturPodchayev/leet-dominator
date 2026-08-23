50de255eff7c740ec774acbcaaf34d091f6d98ef
Artur <142787497+ArturPodchayev@users.noreply.github.com>
2026-08-23 14:35:35 +0500

    Add stableMountains function to find stable indices
    Implement function to find indices of stable mountains above a threshold.

---
+++ find-indices-of-stable-mountains/solution.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} height
+ * @param {number} threshold
+ * @return {number[]}
+ */
+var stableMountains = function(height, threshold) {
+
+    let arr=[]
+
+    for (let i = 0; i < height.length - 1; i++) {
+        if (height[i] > threshold) {
+            arr.push(i + 1)
+        }
+
+    }
+
+    return arr
+
+
+    
+};

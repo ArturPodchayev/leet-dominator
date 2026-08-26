ba56a727b740419453a463777991123c80d7f56f
Artur <142787497+ArturPodchayev@users.noreply.github.com>
2026-08-26 21:18:12 +0500

    Create solution.go

---
+++ find-first-palindromic-string-in-the-array/solution.go
@@ -0,0 +1,20 @@
+func firstPalindrome(words []string) string {
+    for _, w := range words {
+        if isPalindrom(w) {
+            return w
+        }
+    }
+    return ""
+}
+
+func isPalindrom(s string) bool {
+    l, r := 0, len(s)-1
+    for l < r {
+        if s[l] != s[r] {
+            return false
+        }
+        l++
+        r--
+    }
+    return true
+}

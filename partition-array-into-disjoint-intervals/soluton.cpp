976bd771a7c25d6763bd84e0b5932ebd4c48f974
Artur <142787497+ArturPodchayev@users.noreply.github.com>
2026-08-31 17:28:01 +0500

    Create soluton.cpp

---
+++ partition-array-into-disjoint-intervals/soluton.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int partitionDisjoint(vector<int>& a) {
+        int minNum=a[0],maxNum=a[0], index=0;
+        for(int i=1;i<a.size();i++){
+            if(a[i]<minNum){ 
+                index=i;
+                minNum=maxNum;  
+            }
+            maxNum=max(a[i],maxNum);
+        }
+        return index+1;
+    }
+};
